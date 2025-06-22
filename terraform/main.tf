739b07d847ac9f24ad5d139fb31b02982638d246
sistemrobote <sistemrobote@gmail.com>
2025-06-22 12:42:47 -0400

    Secrets for environments

--- terraform/main.tf
+++ terraform/main.tf
@@ -131,7 +131,7 @@ resource "aws_api_gateway_deployment" "deployment" {
 }
 
 resource "aws_api_gateway_stage" "stage" {
-  stage_name    = prod
+  stage_name    = "prod"
   rest_api_id   = aws_api_gateway_rest_api.api.id
   deployment_id = aws_api_gateway_deployment.deployment.id
 }
