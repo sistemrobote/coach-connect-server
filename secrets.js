2734b41f9c2f27311b5fb4e72d9f6ed0c6ef3598
sistemrobote <sistemrobote@gmail.com>
2025-06-22 12:56:11 -0400

    Secrets for environments

--- secrets.js
+++ secrets.js
@@ -6,14 +6,15 @@ const secretsClient = new SecretsManagerClient({ region: process.env.AWS_REGION 
 
 async function getStravaSecrets() {
     console.log(" from secrets >:")
-    if (process.env.NODE_ENV === 'local') {
-        let cachedSecrets = {
-            STRAVA_CLIENT_ID: process.env.STRAVA_CLIENT_ID,
-            STRAVA_CLIENT_SECRET: process.env.STRAVA_CLIENT_SECRET,
-            REDIRECT_URI: process.env.REDIRECT_URI,
-        };
-        return cachedSecrets;
-    }
+    console.log(" process.env.NODE_ENV:", process.env.NODE_ENV)
+    // if ((process?.env?.NODE_ENV && process.env.NODE_ENV) === 'local') {
+    //     let cachedSecrets = {
+    //         STRAVA_CLIENT_ID: process.env.STRAVA_CLIENT_ID,
+    //         STRAVA_CLIENT_SECRET: process.env.STRAVA_CLIENT_SECRET,
+    //         REDIRECT_URI: process.env.REDIRECT_URI,
+    //     };
+    //     return cachedSecrets;
+    // }
     try {
         const command = new GetSecretValueCommand({ SecretId: 'coach-connect-secrets' });
         console.log(" command:", command)
