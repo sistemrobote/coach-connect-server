96cc2952e9b3911d3cc2e667e195928c30a5736e
sistemrobote <sistemrobote@gmail.com>
2025-06-22 12:31:03 -0400

    Secrets for environments

--- secrets.js
+++ secrets.js
@@ -17,7 +17,13 @@ async function getStravaSecrets() {
     }
     try {
         const command = new GetSecretValueCommand({ SecretId: 'coach-connect-secrets' });
+        console.log(" command:", command)
         const response = await secretsClient.send(command);
+        console.log(" response.SecretString:", response.SecretString)
+        if (!response.SecretString) {
+            throw new Error('SecretString missing in Secrets Manager response');
+        }
+        console.log(" JSON.parse(response.SecretString):", JSON.parse(response.SecretString))
         cachedSecrets = JSON.parse(response.SecretString);
         return cachedSecrets;
     } catch (error) {
