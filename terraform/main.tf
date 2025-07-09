789ba2614c43b8b302d08c326d558a6eca745d6b
sistemrobote <sistemrobote@gmail.com>
2025-07-08 20:44:22 -0400

    update policy for DB

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,6 +37,33 @@ data "external" "build_lambda" {
   program = ["bash", "${path.module}/package.sh"]
 }
 
+resource "aws_iam_policy" "lambda_dynamodb_access" {
+  name        = "AllowLambdaDynamoDBAccess-${var.environment}"
+  description = "Allow Lambda to access DynamoDB user_tokens table"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:Query",
+          "dynamodb:Scan"
+        ],
+        Resource = "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/user_tokens"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_dynamodb_access.arn
+}
+
+
 # Attach permissions to Lambda role
 resource "aws_iam_role_policy_attachment" "secrets" {
   role       = aws_iam_role.lambda_exec.name
